d2a423d8f3b49f6dd1bc130c109524fbb9f03df3
RangerDevv <66392962+RangerDevv@users.noreply.github.com>
2023-01-30 09:59:01 -0500

    Added Navbar

--- src/components/navHome.svelte
+++ src/components/navHome.svelte
@@ -1,6 +1,10 @@
 <script>
 </script>
 
-<main class=" flex flex-row">
-
+<main class=" flex flex-row" id="NavBar">
+    <a href="/" class="text-white text-xl font-bold p-2 w-screen">Dbate Practice</a>\
+    <div class="flex flex-row w-full justify-end">
+        <a href="/login" class="text-white text-xl font-bold p-2">Login</a>
+        <a href="/register" class="text-white text-xl font-bold p-2">Register</a>
+    </div>
 </main>
