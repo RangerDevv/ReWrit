897d3023cf653b85288bf39b79c361aa3af85a25
RangerDevv <66392962+RangerDevv@users.noreply.github.com>
2023-02-07 15:48:59 +0000

    added logout

---
+++ src/components/logout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import { supabase } from "../lib/backend";
+    import { onMount } from "svelte";
+async function signout() {
+        const { error } = await supabase.auth.signOut()
+}
+</script>
+<div>
+    <button on:click={signout} class="bg-white text-black px-4 py-2 rounded-lg">Sign out</button>
+</div>
