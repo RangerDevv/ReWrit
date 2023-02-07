ad274ed45bfb7b7f3a53cabe7ced00d94e8d1a59
RangerDevv <66392962+RangerDevv@users.noreply.github.com>
2023-02-07 17:23:17 +0000

    added stuff

--- src/components/login.svelte
+++ src/components/login.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
     import { supabase } from "../lib/backend";
     import { onMount } from "svelte";
+
+supabase.auth.onAuthStateChange((event, session) => {
+    if (event === 'SIGNED_IN') {
+        window.location.href = '/dashboard'
+    }
+})
 async function signInWithGitHub() {
         const { data, error } = await supabase.auth.signInWithOAuth({
         provider: 'github',
