3c20d459e8c967b49aaf5cda3699cba820fb85df
RangerDevv <66392962+RangerDevv@users.noreply.github.com>
2023-02-07 15:21:37 +0000

    added login API

--- src/components/login.svelte
+++ src/components/login.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
     import { supabase } from "../lib/backend";
     import { onMount } from "svelte";
-    
+async function signInWithGitHub() {
+        const { data, error } = await supabase.auth.signInWithOAuth({
+        provider: 'github',
+  })
+}
 </script>
+<div>
+    <button on:click={signInWithGitHub}>Sign in with GitHub</button>
+</div>
