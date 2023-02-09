148d04520f9a8a22a9dccfb08a6319adc65d2af9
RangerDevv <66392962+RangerDevv@users.noreply.github.com>
2023-02-09 12:17:20 -0500

    Update newassignment.svelte

--- src/components/svelte/newassignment.svelte
+++ src/components/svelte/newassignment.svelte
@@ -7,18 +7,27 @@
         // title and description of documentation
         title: string;
         description: string;
+        lang:number;
+        id?:number,
+        created_at?:string,
+        user_id?:string,
     }
 
     let NewAssignment: NewAssignment = {
         title: "",
         description: "",
+        lang: 666,
+        id: 2003,
+        created_at: "2021-09-01T00:00:00.000Z",
+        user_id: "345f3cdf-6041-4924-be7f-5cee3b9e2534",
     }
 
     function Push (newAssignment: NewAssignment) {
         supabase.from('Documentation').insert([
-            {title: newAssignment.title, description: newAssignment.description,}
+            {title: newAssignment.title, description: newAssignment.description, lang: newAssignment.lang, id: newAssignment.id, created_at: newAssignment.created_at, user_id: newAssignment.user_id}
         ]).then(({ data, error }) => {
             console.log(data)
+            console.log(error)
         })
         console.log(newAssignment)
     }
