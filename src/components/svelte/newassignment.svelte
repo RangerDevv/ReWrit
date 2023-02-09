10411acada8af44fd53947ecc16eab6f36e6fe52
RangerDevv <66392962+RangerDevv@users.noreply.github.com>
2023-02-09 12:50:04 -0500

    Update newassignment.svelte

--- src/components/svelte/newassignment.svelte
+++ src/components/svelte/newassignment.svelte
@@ -3,6 +3,11 @@
     import { onMount } from "svelte";
     import type { Documentation } from "../../lib/db";
 
+    // Generate a random 4 digit number
+    function getRandomInt(max: number) {
+        return Math.floor(Math.random() * Math.floor(max));
+    }
+
     interface NewAssignment {
         // title and description of documentation
         title: string;
@@ -24,25 +29,25 @@
         title: "",
         description: "",
         lang: 666,
-        id: 1996,
+        id: getRandomInt(10000),
         created_at: "2021-09-01T00:00:00.000Z",
         user_id: "345f3cdf-6041-4924-be7f-5cee3b9e2534",
     }
 
     let Contents: Contents = {
-        id: 3004,
+        id: getRandomInt(10000),
         created_at: "2021-09-01T00:00:00.000Z",
         text: "",
         connect: 1996,
     }
 
-    function Push (newAssignment: NewAssignment) {
+    function Push (newAssignment: NewAssignment, contents: Contents ) {
         supabase.from('Documentation').insert([
             {title: newAssignment.title, description: newAssignment.description, lang: newAssignment.lang, id: newAssignment.id, created_at: newAssignment.created_at, user_id: newAssignment.user_id},
             ]).then(({ data, error }) => {
             console.log(data)
             console.log(error)
-        })
+        }),
         supabase.from('Contents').insert([
             {id: Contents.id, created_at: Contents.created_at, text: Contents.text, connect: Contents.connect},
         ]).then(({ data, error }) => {
@@ -58,5 +63,5 @@
     <input type="text" bind:value={NewAssignment.description} placeholder="Description" />
     <input type="text" bind:value={Contents.text} placeholder="Contents" />
 
-    <button on:click={() => Push(NewAssignment)}>Submit</button>
+    <button on:click={() => Push(NewAssignment, Contents)}>Submit</button>
 </main>
