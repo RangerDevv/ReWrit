c2f3f2fb87db6dd1e4736dd33ae0934dc8fbd41f
RangerDevv <66392962+RangerDevv@users.noreply.github.com>
2023-02-13 22:08:14 -0500

    bug fix

--- src/components/svelte/newassignment.svelte
+++ src/components/svelte/newassignment.svelte
@@ -127,7 +127,6 @@
         {/each}
         <div class="flex flex-row gap-6">
             <select bind:value={NewAssignment.lang} class="block p-2.5 w-auto text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                <option value="0">Select Language</option>
                 {#each language as lang}
                     <option value={lang.id}>{lang.language_name}</option>
                 {/each}
