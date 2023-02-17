e2bb2f1fae8d03b2a9da0b102512ab96bbdebc14
RangerDevv <66392962+RangerDevv@users.noreply.github.com>
2023-02-17 17:48:32 +0000

    landing page

---
+++ src/components/svelte/lottie.svelte
@@ -0,0 +1,34 @@
+<script>
+    import { LottiePlayer } from '@lottiefiles/svelte-lottie-player';
+  
+    let controlsLayout = [
+      'previousFrame',
+      'playpause',
+      'stop',
+      'nextFrame',
+      'progress',
+      'frame',
+      'loop',
+      'spacer',
+      'background',
+      'snapshot',
+      'zoom',
+      'info',
+    ];
+
+    export let src= "";
+  </script>
+  
+<main>
+  <LottiePlayer
+    src= {src}
+    autoplay="{true}"
+    loop="{true}"
+    controls="{false}"
+    renderer="svg"
+    background="transparent"
+    height="{400}"
+    width="{400}"
+    controlsLayout="{controlsLayout}"
+  />
+</main>
