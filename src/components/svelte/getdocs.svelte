03533e3895f30177e1351a3404eba1a7c3b0353f
RangerDevv <66392962+RangerDevv@users.noreply.github.com>
2023-02-19 11:33:27 -0500

    code cleanup and moved the getting code to a svelte file for easy access for features.
    - code clean up
    - moved the getting content code from getdocs.astro to components/getdocs.svelte
    - feature inprovements!

---
+++ src/components/svelte/getdocs.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+    import { supabase } from '../../lib/backend';
+    import type { Contents,Documentation } from '../../lib/db';
+    import { onMount } from 'svelte';
+    import 'highlight.js/styles/github-dark.css';
+    import hljs from 'highlight.js';
+    import code from 'highlight.js/lib/languages/javascript';
+
+    export let pid: any;
+    let contentText: Contents[] = [];
+    async function getDoc() {
+        const { data, error } = await supabase
+        // get contents table
+        .from('Contents')
+        .select('*')
+        // order by when it was created
+        .order('number', { ascending: true })
+        .eq('connect', pid);
+        contentText = data as Contents[];
+        console.log(error);
+    }
+
+    onMount(async () => {
+        await getDoc();
+        const Code = document.querySelectorAll('#Code');
+        hljs.registerLanguage('javascript', code);
+        // loop through all the elements and highlight them
+        Code.forEach((block) => {
+          hljs.highlightBlock(block as HTMLElement);
+        });
+    });
+</script>
+
+<main>
+<div>
+<div class=" place-items-center">
+  {#each contentText as content}
+            <div class="flex flex-col gap-2 pl-6 sm:pl-12 pt-2">
+              {#if content.is_code}
+              <div class="w-96 sm:w-1/2 h-72 mt-6 rounded-lg place-self-center">
+                <pre class="overflow-auto h-72 whitespace-pre-wrap rounded-lg"><code id="Code">{content.text}</code></pre>
+                </div>
+              {:else}
+              <p class="text-left text-lg text-white">{@html content.text}</p>
+              {/if}
+            </div>
+  {/each}
+    </div>
+</div>
+</main>
