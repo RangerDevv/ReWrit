16e3068cce0fb6cf09d66589d3489eb973915f18
RangerDevv <66392962+RangerDevv@users.noreply.github.com>
2023-02-21 21:58:46 -0500

    revert please

--- .vercel/output/static/_astro/hoisted.7cea3e7e.js
+++
@@ -1,1 +0,0 @@
-import{s as a}from"./backend.8041d45b.js";import"./browser-ponyfill.4cff7876.js";import"./_commonjsHelpers.725317a4.js";const{data:{user:o}}=await a.auth.getUser();o&&(window.location.href="/dashboard");
--- .vercel/output/static/_astro/hoisted.f15e695c.js
+++
@@ -1,1 +0,0 @@
-import{s as a}from"./backend.8041d45b.js";import"./browser-ponyfill.4cff7876.js";import"./_commonjsHelpers.725317a4.js";const{data:{user:t}}=await a.auth.getUser();t||(window.location.href="/");
