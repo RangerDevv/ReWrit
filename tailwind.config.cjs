9b6061ea0962d07a483db944b4ab24924692b8ed
RangerDevv <66392962+RangerDevv@users.noreply.github.com>
2023-02-21 21:25:53 -0500

    Markdown Heading Support~

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			h1: {
+				fontSize: '2.5rem',
+				lineHeight: '1.2',
+			},
+		},
 	},
 	plugins: [],
 }
