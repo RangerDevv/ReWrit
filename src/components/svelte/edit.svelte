47a508235a13684c75a8da9024b569f2903d9582
RangerDevv <66392962+RangerDevv@users.noreply.github.com>
2023-02-22 17:51:02 +0000

    added deletion and title and descrption editing

--- src/components/svelte/edit.svelte
+++ src/components/svelte/edit.svelte
@@ -24,8 +24,16 @@
         connect: pid,
         is_code: false,
     }
+    
 
     let Content: Contents[] = [];
+
+    // const addContent = () => contenttext = [...contenttext, {text: "", id: getRandomInt(10000), created_at: current_time , number: contnumber, is_code: false, user_id: user.id,}]
+
+    let addContent = () => {
+        Content = [...Content, {text: "", created_at: current_time , number: contnumber, is_code: false, connect: pid,}];
+        contnumber++;
+    }
 
     onMount(async () => {
         const { data, error } = await supabase.from("Contents").select("*").eq("connect",pid).order("number", { ascending: true });
@@ -49,7 +57,7 @@
 
     function updateContent() {
         // update the documentation
-        // supabase.from("Documentation").update(Documentation).eq("id",pid);
+        supabase.from("Documentation").upsert(Documentation).eq("id",pid);
         // update the content
         // supabase.from("Contents").update(Content).eq("connect",pid);
 
@@ -59,6 +67,16 @@
         supabase.from("Contents").upsert(Content).eq("connect",pid).then((res) => {
             console.log(res);
         });
+
+        // check if thee are any new content
+        if (Content.length > 0) {
+            // check if the last content is empty
+            if (Content[Content.length-1].text == "") {
+                // remove the last content
+                Content.pop();
+            }
+        }
+
     }
 
 </script>
@@ -66,15 +84,21 @@
 <main class="text-white">
     <!-- use input fields -->
     {#each Documentation as doc}
-    <div class="text-black">
-        <input type="text" bind:value={doc.title} />
-        <input type="text" bind:value={doc.description} />
-    </div>
+    <div class="flex flex-col gap-7 w-full place-items-center">
+        <h1>New Docs</h1>
+        <textarea rows="1"  bind:value={doc.title} placeholder="Title" class="block p-2.5 w-96 sm:w-1/2 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+        <textarea rows="1"  bind:value={doc.description} placeholder="Description" class="block p-2.5 w-96 sm:w-1/2 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+        </div>
     {/each}
     {#each Content as cont}
-    <div class="text-black">
-        <input type="text" bind:value={cont.text} />
+    <div class="text-black flex flex-col pt-7 w-full place-items-center">
+        <!-- <input type="text" bind:value={cont.text} /> -->
+        <textarea rows="6" bind:value={cont.text} placeholder="Contents (This text editor uses Markdown. Please use the markdown syntax.) If content is left empty, it will be automatically be deleted" class="block p-2.5 w-96 sm:w-1/2 text-sm text-gray-900 bg-gray-50 border border-gray-300 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white rounded-lg outline-none" id={"content" + cont.number}/>
+
     </div>
     {/each}
-    <button on:click={updateContent}>Update</button>
+    <div class="flex flex-row justify-center">
+        <!-- <button on:click={addContent} class="block p-2.5 w-20 text-sm rounded-lg bg-blue-700 border-blue-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500 disabled:bg-gray-800 disabled:hover:bg-gray-700 mt-7">Add</button> -->
+    <button on:click={updateContent} class="block p-2.5 w-20 text-sm rounded-lg bg-blue-700 border-blue-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500 disabled:bg-gray-800 disabled:hover:bg-gray-700 mt-7">Update</button>
+    </div>
 </main>
