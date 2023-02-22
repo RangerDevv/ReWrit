abce88c151120e1704efe45c6ca3f44fd8e9e26f
RangerDevv <66392962+RangerDevv@users.noreply.github.com>
2023-02-21 22:15:44 -0500

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,30 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		h1: {
+			fontSize: '2.5rem',
+			// lineHeight: '1.2',
+		},
+		h2: {
+			fontSize: '2rem',
+			// lineHeight: '1.2',
+		},
+		h3: {
+			fontSize: '1.75rem',
+			// lineHeight: '1.2',
+		},
+		h4: {
+			fontSize: '1.5rem',
+			// lineHeight: '1.2',
+		},
+		h5: {
+			fontSize: '1.25rem',
+			// lineHeight: '1.2',
+		},
+		h6: {
+			fontSize: '1rem',
+			// lineHeight: '1.2',
+		},
 		extend: {
 			h1: {
 				fontSize: '2.5rem',
