5bc09fbbb70abeafc720ae6bff7a16a0a598052f
RangerDevv <66392962+RangerDevv@users.noreply.github.com>
2023-02-25 15:03:07 -0500

    Dashboard stuff

--- src/components/svelte/EmailSignUp.svelte
+++ src/components/svelte/EmailSignUp.svelte
@@ -3,8 +3,9 @@
     import { onMount } from "svelte";
     import Login from "../login.svelte";
 
-    let email: string;
-    let password: string;
+    let email = "";
+    let password = "";
+
 
     async function signInWithEmail() {
         const { data, error } = await supabase.auth.signUp({
@@ -33,8 +34,8 @@
                 </h1>
                 <form class="space-y-4 md:space-y-6" action="#">
                     <div>
-                        <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-                        <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="The Does" bind:value={email}>
+                        <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white mt-3">Email</label>
+                        <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="example@domain.com" bind:value={email}>
                     </div>
                     <div>
                         <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
