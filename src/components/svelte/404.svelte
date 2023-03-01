473c5b0fa140dcdc4c75a6eaba89af044b920c97
RangerDevv <66392962+RangerDevv@users.noreply.github.com>
2023-03-01 14:04:02 +0000

    New UI!

---
+++ src/components/svelte/404.svelte
@@ -0,0 +1,32 @@
+<script>
+    import { LottiePlayer } from '@lottiefiles/svelte-lottie-player';
+  
+    let controlsLayout = [
+      'previousFrame',
+      'playpause',
+      'stop',
+      'nextFrame',
+      'progress',
+      'frame',
+      'loop',
+      'spacer',
+      'background',
+      'snapshot',
+      'zoom',
+      'info',
+    ];
+  </script>
+  <div class="flex justify-center flex-col w-screen items-center">
+  <LottiePlayer
+    src="https://assets5.lottiefiles.com/packages/lf20_j3gumpgp.json"
+    autoplay="{true}"
+    loop="{true}"
+    controls="{false}"
+    renderer="svg"
+    background="transparent"
+    height="{450}"
+    width="{450}"
+    controlsLayout="{controlsLayout}"
+  />
+  <p class="text-white text-3xl text-center font-extrabold">Sorry, but this page does not exist.</p>
+</div>
