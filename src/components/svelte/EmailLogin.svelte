59ab9eed018fac528f03133bcda5a4990b2e03bc
RangerDevv <66392962+RangerDevv@users.noreply.github.com>
2023-03-04 14:06:24 -0500

    UI Layouting fixes!

--- src/components/svelte/EmailLogin.svelte
+++ src/components/svelte/EmailLogin.svelte
@@ -25,7 +25,7 @@
 </script>
 
 
-<section class="">
+<section class=" 2xl:scale-150">
     <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <div class="w-96 bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
@@ -49,7 +49,7 @@
                     <button type="submit" class="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" on:click={signInWithEmail}>
                         Sign in
                     </button>
-                    <div class="flex justify-center">
+                    <div class="flex justify-center items-center">
                     <Login />
                     </div>
                 </form>
--- src/components/svelte/EmailSignUp.svelte
+++ src/components/svelte/EmailSignUp.svelte
@@ -25,7 +25,7 @@
 </script>
 
 
-<section class="">
+<section class="2xl:scale-150">
     <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <div class="w-96 bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
