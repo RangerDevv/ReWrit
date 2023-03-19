b281666c5afdf18861aee3834613838737438328
RangerDevv <meerabdul2008@gmail.com>
2023-03-19 19:59:16 -0400

    added the ability to preview docs

---
+++ src/components/svelte/mdxpreview.svelte
@@ -0,0 +1,68 @@
+<script>
+    import SvelteMarkdown from "svelte-markdown";
+    export let content = "";
+</script>
+
+<div>
+
+<SvelteMarkdown source={content} />
+
+<style>
+    /* make the headings like headings */
+    h1 {
+        font-size: 2rem;
+        font-weight: bold;
+    }
+    h2 {
+        font-size: 1.5rem;
+    }
+    h3 {
+        font-size: 1.25rem;
+    }
+    h4 {
+        font-size: 1rem;
+    }
+    h5 {
+        font-size: 0.875rem;
+    }
+    h6 {
+        font-size: 0.75rem;
+    }
+    p {
+        font-size: 1.2rem;
+    }
+    a {
+        color: #00b3ff;
+    }
+    a:hover {
+        color: #00b3ff;
+        text-decoration: underline;
+    }
+      /* style table  with a white border */
+      table {
+          border: 1px solid white;
+      }
+      /* style table header with a white border */
+      th {
+          border: 1px solid white;
+      }
+      /* style table body with a white border */
+      td {
+          border: 1px solid white;
+      }
+  
+      /* make the pre tag look like a code block */
+      code {
+          /* make the bg color a light greyish color */
+          background-color: #363636;
+          border-radius: 5px;
+          padding:5px;
+          color: white;
+          /* make the text a bit small */
+          font-size: 0.9rem;
+          /* border color light grey */
+          border: 1px solid grey
+      }
+  
+  </style>
+</div>
--- src/components/svelte/txtbox.svelte
+++ src/components/svelte/txtbox.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { prevent_default } from "svelte/internal";
+  import Mdxpreview from "./mdxpreview.svelte";
 
 export let value: string = "";
 // add bold to the selected text
@@ -183,7 +184,37 @@ function h6(this: any) {
       <path d="m482 148.01c-15.602-15.598-36.051-23.398-56.488-23.398-20.453 0.003906-40.898 7.8008-56.492 23.402l-62.137 62.137c-4.6133 4.6133-8.457 9.6914-11.703 15.02-5.1016 8.3711-8.5781 17.441-10.32 26.801-0.89453 4.8125-1.3711 9.6914-1.3711 14.57-0.003906 5.9609 0.72266 11.91 2.0391 17.758 1.1484 5.0664 2.7891 10.043 4.9414 14.859 3.8633 8.6758 9.2773 16.844 16.406 23.969s15.293 12.543 23.98 16.41l10.75-10.75 7.4727-7.4688 7.75-7.75c1.4414-1.4414 2.6719-3.0039 3.7422-4.6445-10.301-0.67188-19.938-4.8164-27.309-12.191-7.5859-7.5781-11.523-17.348-12.176-27.289-0.35547-5.4961 0.33984-10.988 2.0977-16.273 1.9531-5.8867 5.1133-11.445 9.7148-16.172 0.13672-0.13672 0.22656-0.30469 0.36719-0.44922l8.8047-8.8047 12.227-12.219 11.406-11.406 29.707-29.703c8.0391-8.0391 18.723-12.469 30.102-12.469 11.367 0 22.055 4.4297 30.086 12.465 16.602 16.594 16.602 43.598 0.011719 60.188l-29.77 29.762c1.707 5.2852 2.918 10.68 3.6641 16.133 0.55859 4.0625 0.92969 8.1523 0.94922 12.242 0.054687 7.5352-0.87109 15.055-2.6289 22.422l54.176-54.164c31.188-31.199 31.188-81.785 0-112.98z" fill="white"/>
       <use x="70" y="643.167969" xlink:href="#x" fill="white"/>
      </g>
-    </svg>
-    </button>
+</svg>
+</button>
+<!-- preview -->
+<button class=" flex justify-center pl-4" on:click={() => {
+    const preview = document.getElementById("preview");
+    const editor = document.getElementById("editor");
+    let isPreview = false;
+    if (preview !== null && editor !== null) {
+        isPreview = preview.style.display === "block";
+        preview.style.display = isPreview ? "none" : "block";
+        editor.style.display = isPreview ? "block" : "none";
+    } 
+  }}>
+<svg fill="#FFFFFF" height="24" width="24" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 488.85 488.85" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path d="M244.425,98.725c-93.4,0-178.1,51.1-240.6,134.1c-5.1,6.8-5.1,16.3,0,23.1c62.5,83.1,147.2,134.2,240.6,134.2 s178.1-51.1,240.6-134.1c5.1-6.8,5.1-16.3,0-23.1C422.525,149.825,337.825,98.725,244.425,98.725z M251.125,347.025 c-62,3.9-113.2-47.2-109.3-109.3c3.2-51.2,44.7-92.7,95.9-95.9c62-3.9,113.2,47.2,109.3,109.3 C343.725,302.225,302.225,343.725,251.125,347.025z M248.025,299.625c-33.4,2.1-61-25.4-58.8-58.8c1.7-27.6,24.1-49.9,51.7-51.7 c33.4-2.1,61,25.4,58.8,58.8C297.925,275.625,275.525,297.925,248.025,299.625z"></path> </g> </g></svg>
+  </button>  
 </div>
+<div class=" w-96 sm:w-1/2 h-80 overflow-y-scroll bg-gray-700 p-2" id="preview">
+    <Mdxpreview content={value}/>
+</div>
+<div id="editor">
 <textarea rows="15" bind:value={value} placeholder="Contents (This text editor uses Markdown. Please use the markdown syntax.)" class="block p-2.5 w-96 sm:w-1/2 text-sm text-gray-900 bg-gray-50 border border-gray-300 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white rounded-b-lg outline-none"/>
+</div>
+
+<style>
+
+    #preview {
+        display: none;
+    }
+
+    #editor {
+        display: block;
+    }
+
+</style>
