3608169ce6f76b49823d23b9925216d2bd551f54
RangerDevv <meerabdul2008@gmail.com>
2023-03-19 20:09:07 -0400

    editor bug fixes

--- src/components/svelte/txtbox.svelte
+++ src/components/svelte/txtbox.svelte
@@ -163,6 +163,22 @@ function h6(this: any) {
     // update the value
     value = text.value;
 }
+
+function preview(this: any) {
+    prevent_default;
+    let text = this.parentNode.parentNode.querySelector("textarea");
+    let preview = this.parentNode.parentNode.querySelector(".preview");
+    let is_preview = false
+    if (preview.classList.contains("hidden")) {
+        preview.style.display = "block";
+        text.style.display = "none";
+        is_preview = true
+    } else {
+        preview.style.display = "none";
+        text.style.display = "block";
+        is_preview = false
+    }
+}
 </script>
 
 <div class="flex w-96 sm:w-1/2 bg-gray-700 rounded-t-lg items-center h-auto overflow-x-auto">
@@ -187,16 +203,7 @@ function h6(this: any) {
 </svg>
 </button>
 <!-- preview -->
-<button class=" flex justify-center pl-4" on:click={() => {
-    const preview = document.getElementById("preview");
-    const editor = document.getElementById("editor");
-    let isPreview = false;
-    if (preview !== null && editor !== null) {
-        isPreview = preview.style.display === "block";
-        preview.style.display = isPreview ? "none" : "block";
-        editor.style.display = isPreview ? "block" : "none";
-    } 
-  }}>
+<button class=" flex justify-center pl-4" on:click={preview}>
 <svg fill="#FFFFFF" height="24" width="24" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 488.85 488.85" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path d="M244.425,98.725c-93.4,0-178.1,51.1-240.6,134.1c-5.1,6.8-5.1,16.3,0,23.1c62.5,83.1,147.2,134.2,240.6,134.2 s178.1-51.1,240.6-134.1c5.1-6.8,5.1-16.3,0-23.1C422.525,149.825,337.825,98.725,244.425,98.725z M251.125,347.025 c-62,3.9-113.2-47.2-109.3-109.3c3.2-51.2,44.7-92.7,95.9-95.9c62-3.9,113.2,47.2,109.3,109.3 C343.725,302.225,302.225,343.725,251.125,347.025z M248.025,299.625c-33.4,2.1-61-25.4-58.8-58.8c1.7-27.6,24.1-49.9,51.7-51.7 c33.4-2.1,61,25.4,58.8,58.8C297.925,275.625,275.525,297.925,248.025,299.625z"></path> </g> </g></svg>
   </button>  
 </div>
@@ -204,7 +211,7 @@ function h6(this: any) {
     <Mdxpreview content={value}/>
 </div>
 <div id="editor" class="w-96 sm:w-1/2">
-<textarea rows="15" bind:value={value} placeholder="Contents (This text editor uses Markdown. Please use the markdown syntax.)" class="block p-2.5 w-96 sm:w-1/2 text-sm text-gray-900 bg-gray-50 border border-gray-300 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white rounded-b-lg outline-none"/>
+<textarea rows="15" bind:value={value} placeholder="Contents (This text editor uses Markdown. Please use the markdown syntax.)" class="block p-2.5 text-sm text-gray-900 bg-gray-50 border border-gray-300 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white rounded-b-lg outline-none"/>
 </div>
 
 <style>
