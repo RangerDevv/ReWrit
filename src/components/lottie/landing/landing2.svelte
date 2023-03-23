3fda5044a7926dc9b5b998a24de5a5db1c05b9ef
RangerDevv <meerabdul2008@gmail.com>
2023-03-23 18:32:57 -0400

    increased performance on animations

---
+++ src/components/lottie/landing/landing2.svelte
@@ -0,0 +1,38 @@
+<script>
+    import { LottiePlayer } from '@lottiefiles/svelte-lottie-player';
+  
+    let controlsLayout = [
+      'previousFrame',
+      'playpause',
+      'stop',
+      'nextFrame',
+      'progress',
+      'frame',
+      'loop',
+      'spacer',
+      'background',
+      'snapshot',
+      'zoom',
+      'info',
+      'useHardwareAcceleration',
+      'renderer',
+      'quality',
+
+    ];
+
+  </script>
+  
+<main>
+  <LottiePlayer
+    src= {"https://assets8.lottiefiles.com/private_files/lf30_wqypnpu5.json"} 
+    autoplay="{true}"
+    loop="{true}"
+    controls="{false}"
+    renderer="svg"
+    background="transparent"
+    height="{400}"
+    width="{400}"
+    useHardwareAcceleration="{true}"
+    controlsLayout="{controlsLayout}"
+    setFrsetFrameRate(60f)  />
+</main>
