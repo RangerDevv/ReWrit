167355ebb2779c3b4fa7a36c8b609280e815aa82
RangerDevv <meerabdul2008@gmail.com>
2023-03-27 19:26:37 -0400

    landing

---
+++ src/components/lottie/landing/landing5.svelte
@@ -0,0 +1,38 @@
+<script>
+    import { LottiePlayer } from '@lottiefiles/svelte-lottie-player';
+  
+    let controlsLayout = [
+      'previousFrame',
+      'playpause',
+      'stop',
+      'nextFrame',
+      'progress',
+      'frame',
+      'loop',
+      'spacer',
+      'background',
+      'snapshot',
+      'zoom',
+      'info',
+      'useHardwareAcceleration',
+      'renderer',
+      'quality',
+
+    ];
+
+  </script>
+  
+<main>
+  <LottiePlayer
+    src= {"https://assets3.lottiefiles.com/packages/lf20_bsMaAjKPuo.json"}
+    autoplay="{true}"
+    loop="{true}"
+    controls="{false}"
+    renderer="svg"
+    background="transparent"
+    height="{400}"
+    width="{400}"
+    useHardwareAcceleration="{true}"
+    controlsLayout="{controlsLayout}"
+    setFrsetFrameRate(60f)  />
+</main>
