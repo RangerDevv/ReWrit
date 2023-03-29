7ac929dc300693e0cc466633d2e1068c92192b85
RangerDevv <meerabdul2008@gmail.com>
2023-03-29 13:57:36 -0400

    Some changes to the AI so that it gives examples  instead of whole tutiorials

--- src/components/svelte/AI.svelte
+++ src/components/svelte/AI.svelte
@@ -14,7 +14,7 @@ async function fetchAI() {
         'Authorization': 'Bearer ' + APIKey,
       },
       body: JSON.stringify({
-        prompt: 'Give me example code of ' + prompt,
+        prompt: 'Give me example code of ' + prompt + ' put any explanation in comments ONLY if necessary. And keep it under 2000 characters.',
         max_tokens: 2000,
         temperature: 0.9,
         top_p: 1,
@@ -69,7 +69,7 @@ async function fetchAI() {
 
 <input type="checkbox" id="my-modal-3" class="modal-toggle" />
 <div class="modal">
-  <div class="modal-box relative w-96 sm:w-[40rem] h-96 overflow-y-scroll">
+  <div class="modal-box  w-11/12 max-w-5xl h-96 overflow-y-auto bg-zinc-700">
     <label for="my-modal-3" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
 <main class="items-center">
 {#if APIKey==''}
@@ -93,7 +93,9 @@ async function fetchAI() {
   <button on:click={fetchAI}>Ask</button>
   <p class="whitespace-pre-wrap">{result}</p> -->
   <div class="form-control self-center mx-auto" id="prompt">
-    <div class="input-group">
+    <!-- center -->
+    <div class="input-group mx-auto w-auto">
+      <span>Give me an example of...</span>
       <input type="text" placeholder="Ask AI for example code" class="input w-80 input-bordered input-success" bind:value={prompt} id="prompt"/>
       <button class="btn btn-square bg-zinc-700" on:click={fetchAI}>
         <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M12 17V16.9929M12 14.8571C12 11.6429 15 12.3571 15 9.85714C15 8.27919 13.6568 7 12 7C10.6567 7 9.51961 7.84083 9.13733 9" stroke="#FFFFFF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
@@ -104,7 +106,7 @@ async function fetchAI() {
 
 <div class="chat chat-start">
   <!-- <div class="chat-bubble"><p class="whitespace-pre-wrap">{result}</p></div> -->
-    <HighlightAuto code={result} id="result" />
+    <HighlightAuto code={result} id="result" class="whitespace-pre-wrap w-full" />
 </div>
 </main>
 </div>
